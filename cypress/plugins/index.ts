143247291a9f05b98ff4d77478e1c2eb68adfda1
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-21 14:25:50 +0200

    Fix lint

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -15,5 +15,7 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
-module.exports = () => {};
+module.exports = () => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+};
