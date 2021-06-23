7ec08525de0cfbe2ec92fe63032e4c90c317e70c
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-23 20:42:13 +0200

    wait

--- cypress/integration/cart.spec.ts
+++ cypress/integration/cart.spec.ts
@@ -9,18 +9,20 @@ describe('Cart tests', () => {
   });
 
   it('Devrait afficher les 3 premiers livres dans le panier', () => {
-    cy.get('.addBookToCartButton')
-      .eq(0)
-      .click()
-      .get('.addBookToCartButton')
-      .eq(1)
-      .click()
-      .get('.addBookToCartButton')
-      .eq(2)
-      .click()
-      .get('.cartItem')
-      .should('have.length', 3)
-      .get('#totalPrice')
-      .should('have.text', 'Total : 95');
+    cy.wait('@getBooks').then(() => {
+      cy.get('.addBookToCartButton')
+        .eq(0)
+        .click()
+        .get('.addBookToCartButton')
+        .eq(1)
+        .click()
+        .get('.addBookToCartButton')
+        .eq(2)
+        .click()
+        .get('.cartItem')
+        .should('have.length', 3)
+        .get('#totalPrice')
+        .should('have.text', 'Total : 95');
+    });
   });
 });
