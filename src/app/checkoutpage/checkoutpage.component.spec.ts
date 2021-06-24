28e70411746d4366fcf226e6117dd7fb8935be24
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-24 13:51:54 +0200

    ajout d'imports manquants

--- src/app/checkoutpage/checkoutpage.component.spec.ts
+++ src/app/checkoutpage/checkoutpage.component.spec.ts
@@ -1,7 +1,9 @@
+import { ApiService } from './../services/api.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CheckoutpageComponent } from './checkoutpage.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CheckoutpageComponent', () => {
   let component: CheckoutpageComponent;
@@ -19,8 +21,11 @@ describe('CheckoutpageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CheckoutpageComponent],
-      providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }],
-      imports: [RouterTestingModule.withRoutes([])]
+      providers: [
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute },
+        ApiService
+      ],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([])]
     }).compileComponents();
   });
 
