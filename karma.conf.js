9d9aaec83a7e36ebfc659b034c8fb79acdc6200f
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-25 10:59:28 +0200

    test multiple paths

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './karma-coverage'),
       subdir: '.',
-      reporters: [{ type: 'json' }]
+      reporters: [{ type: 'json' }, { type: 'lcov' }]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
