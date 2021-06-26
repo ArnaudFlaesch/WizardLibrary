9b65fd8bf9062599e8f4a916c9d00bd38badeea7
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-06-26 20:03:47 +0200

    CR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { ManageOrdersPageComponent } from './manage-orders-page/manage-orders-page.component';
-import { BookListComponent } from './book-list-page/book-list-page.component';
+import { BookListPageComponent } from './book-list-page/book-list-page.component';
 import { CheckoutpageComponent } from './checkout-page/checkout-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', component: BookListComponent },
+  { path: 'home', component: BookListPageComponent },
   { path: 'checkout', component: CheckoutpageComponent },
   { path: 'manageOrders', component: ManageOrdersPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
