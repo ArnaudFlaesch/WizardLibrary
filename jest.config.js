c09de2a20d92e0318d3e6247bbaaac20bfbf8664
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-10-05 15:39:33 +0200

    Replace Karma with Jest, add spectator, use Yarn

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line no-undef
 require('jest-preset-angular/ngcc-jest-processor');
 
+// eslint-disable-next-line no-undef
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['./setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
   collectCoverage: true,
   coverageReporters: ['html', 'lcov'],
-  coverageDirectory: 'coverage/'
+  coverageDirectory: 'coverage-jest/'
 };
